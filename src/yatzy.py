4a387b28f9d8f08122e9d59033216b5e0f6357d4
davidbua <bua.teijeiro.david@gmail.com>
2024-01-08 10:09:52 +0100

    refactor(yatzy.py): Modificar la función ones añadiendo un bucle y corregir casos test

--- src/yatzy.py
+++ src/yatzy.py
@@ -24,20 +24,14 @@ class Yatzy:
         return 0
     
     @staticmethod
-    def ones( d1,  d2,  d3,  d4,  d5):
-        sum = 0
-        if (d1 == 1):
-            sum += 1
-        if (d2 == 1):
-            sum += 1
-        if (d3 == 1):
-            sum += 1
-        if (d4 == 1):
-            sum += 1
-        if (d5 == 1): 
-            sum += 1
-
-        return sum
+    def ones( *dice):
+        sum_ones = 0
+        for die in dice:
+            if die == 1:
+                sum_ones += die
+            else:
+                continue
+        return sum_ones
     
 
     @staticmethod
